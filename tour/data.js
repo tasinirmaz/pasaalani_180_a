f639e63e156785ed44919bd5f9b751fc5d7a4d5e
tasinIRmaz <keskinrecep26@gmail.com>
2025-11-10 17:50:19 +0300

    d

--- tour/data.js
+++ tour/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-karakaya",
-      "name": "karakaya",
+      "id": "0-20251110_173739_838",
+      "name": "20251110_173739_838",
       "levels": [
         {
           "tileSize": 256,
@@ -22,76 +22,17 @@ var APP_DATA = {
           "size": 2048
         }
       ],
-      "faceSize": 2048,
+      "faceSize": 2000,
       "initialViewParameters": {
-        "yaw": 0.1821936445106438,
-        "pitch": 0.3556540610571126,
-        "fov": 1.2599180821480807
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
       },
-      "linkHotspots": [
-        {
-          "yaw": -0.1757219636188605,
-          "pitch": 0.5491282937901971,
-          "rotation": 0,
-          "target": "1-kayack"
-        }
-      ],
-      "infoHotspots": [
-        {
-          "yaw": -0.8193059419904483,
-          "pitch": 0.33285464251583896,
-          "title": "Salon",
-          "text": "6,5M - 4M (26M²)"
-        }
-      ]
-    },
-    {
-      "id": "1-kayack",
-      "name": "kayacık",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": 0.2862463929206278,
-        "pitch": 0.6931549157156507,
-        "fov": 1.2599180821480807
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.2862463929206278,
-          "pitch": 0.6931549157156507,
-          "rotation": 0,
-          "target": "0-karakaya"
-        }
-      ],
-      "infoHotspots": [
-        {
-          "yaw": -0.6493463031220585,
-          "pitch": 0.565619960767636,
-          "title": "Mutfak",
-          "text": "4M - 3M (12M²)"
-        }
-      ]
+      "linkHotspots": [],
+      "infoHotspots": []
     }
   ],
-  "name": "bahcelievler_tekniklise_160_1",
+  "name": "Project Title",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
